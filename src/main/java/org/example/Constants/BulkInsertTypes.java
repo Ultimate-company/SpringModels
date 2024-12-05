0c627d8da3ed52160e0cb736f22dadcb33dede52
nahushr18 <nahushrai@hotmail.com>
2024-12-04 21:14:05 -0500

    File need for PR #9 (#10)
    * work in progress
    
    * final changes everything done
    
    * not needed
    
    * this is needed

---
+++ src/main/java/org/example/Constants/BulkInsertTypes.java
@@ -0,0 +1,16 @@
+package org.example.Constants;
+
+public class BulkInsertTypes {
+    public static final String USER = "User";
+    public static final String LEAD = "Lead";
+    public static final String MESSAGE = "Message";
+    public static final String PACKAGE = "Package";
+    public static final String PICKUP_LOCATION = "PickupLocation";
+    public static final String PRODUCT = "Product";
+    public static final String PROMO = "Promo";
+    public static final String PURCHASE_ORDER = "PurchaseOrder";
+    public static final String SALES_ORDER = "SalesOrder";
+    public static final String SUPPORT = "Support";
+    public static final String USER_GROUP = "UserGroup";
+    public static final String WEB_TEMPLATE = "WebTemplate";
+}
